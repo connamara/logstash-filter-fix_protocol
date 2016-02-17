fb8d93e48799e6015138355011f778b5013330a5
daino3 <dainovu3@gmail.com>
2016-02-17 12:41:28 -0600

    Moving data dictionary xml parsing into spec helper module

--- lib/logstash/filters/data_dictionary.rb
+++ lib/logstash/filters/data_dictionary.rb
@@ -1,45 +1,14 @@
 require 'quickfix'
-require 'rexml/document'
 
 module LogStash
   module  Filters
     class DataDictionary < quickfix.DataDictionary
-      attr_reader :file, :reverse_lookup
+      attr_reader :file
 
       def initialize(file_path)
+        @file = ::File.new(file_path) # throw an exception if the file isn't found
+
         super(file_path)
-
-        @file = ::File.new(file_path)
-        @reverse_lookup = ::Hash.new
-        parse_xml
-      end
-
-      def get_reverse_value_name(tag, name)
-        if @reverse_lookup[tag].has_key?(name)
-          @reverse_lookup[tag][name]
-        else
-          name
-        end
-      end
-
-      private
-
-      def parse_xml
-        doc = ::REXML::Document.new(@file)
-
-        doc.elements.each("fix/fields/field") do |f|
-          tag = f.attributes['number'].to_i
-          @reverse_lookup[tag] ||= {}
-
-          f.elements.each("value") do |v|
-            @reverse_lookup[ tag ][v.attributes['description']] = v.attributes['enum']
-          end
-        end
-
-        #also map pretty msg type names
-        doc.elements.each("fix/messages/message") do |m|
-          @reverse_lookup[35][m.attributes['name']] = m.attributes['msgtype']
-        end
       end
     end
   end
